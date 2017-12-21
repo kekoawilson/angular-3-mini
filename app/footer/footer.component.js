f26f2453b5778aa075aa88480a855c0eb684a6f6
Kekoa Wilson <kekoawilson20@gmail.com>
2017-12-21 14:25:32 -0700

    first commit

---
+++ app/footer/footer.component.js
@@ -0,0 +1,15 @@
+angular.module('fourWheels').component('footer', {
+    templateUrl: 'app/footer/footer.template.html',
+    controllerAs: 'footerCtrl',
+    controller: function() {
+        this.name = ''
+        this.email = ''
+        this.contacted = false
+        
+        this.contact = () => {
+            this.contacted = true
+            this.name = ''
+            this.email = ''
+        }
+    }
+})
